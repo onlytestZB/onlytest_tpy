070c6a4140ca41c1e8641f2a7580c5788074e1c4
zb3215286@hotmail.com <3219286a>
2018-01-24 15:25:44 +0800

    4.5

--- jump.py
+++ jump.py
@@ -1,15 +1,16 @@
 from abupy import ABuSymbolPd
+from abupy import ABuMarketDrawing
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 tsla_df = ABuSymbolPd.make_kl_df('usTSLA',n_folds=2)
 jump_threshold = tsla_df.close.median()*0.03
 print(jump_threshold)
-print(tsla_df)
+#print(tsla_df)
 jump_pd = pd.DataFrame()
 def judge_jump(today):
     global jump_pd
-    if today.p_change>0 and (today.low-today.pre_cXlose)>jump_threshold:
+    if today.p_change>0 and (today.low-today.pre_close)>jump_threshold:
         """
         符合向上跳空
         """
@@ -28,3 +29,4 @@ for kl_index in np.arange(0,tsla_df.shape[0]):
     judge_jump(today)
 print(jump_pd.filter(['jump','jump_power','close','date',
                       'p_change','pre_close']))
+ABuMarketDrawing.plot_candle_form_klpd(tsla_df,view_indexs=jump_pd.index)
