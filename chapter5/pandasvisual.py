d4d180bf3bd76c91d4b3628efe16d7e290dfc61a
onlytestzb <zb3215286@hotmail.com>
2018-01-28 13:42:33 +0800

    5.3.2

--- chapter5/pandasvisual.py
+++ chapter5/pandasvisual.py
@@ -4,7 +4,8 @@ import pandas as pd
 from abupy import ABuSymbolPd
 import matplotlib.finance as mpf
 tsla_df = ABuSymbolPd.make_kl_df('usTSLA',n_folds=2)
-
+'''
+5.3.1
 tsla_df_copy = tsla_df.copy()
 tsla_df_copy['return'] = np.log(tsla_df['close']/tsla_df['close'].shift(1))
 tsla_df_copy['mov_std'] = pd.rolling_std(tsla_df_copy['return'],
@@ -16,3 +17,11 @@ tsla_df_copy['std_ewm'] = pd.ewmstd(tsla_df_copy['return'],span=20,
 tsla_df_copy[['close','mov_std','std_ewm','return']].plot(
     subplots = True,grid = True)
 plt.show()
+5.3.2
+tsla_df.close.plot()
+pd.rolling_mean(tsla_df.close,window=30).plot()
+pd.rolling_mean(tsla_df.close,window=60).plot()
+pd.rolling_mean(tsla_df.close,window=90).plot()
+plt.legend(['close','30 mv','60 mv','90 mv'],loc='best')
+plt.show()
+'''
