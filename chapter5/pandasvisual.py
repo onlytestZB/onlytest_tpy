8da3b6c1ff35f88325c7f36bf8bf3c39c10d6bfc
onlytestzb <zb3215286@hotmail.com>
2018-01-28 14:20:55 +0800

    5.3.3

--- chapter5/pandasvisual.py
+++ chapter5/pandasvisual.py
@@ -25,3 +25,24 @@ pd.rolling_mean(tsla_df.close,window=90).plot()
 plt.legend(['close','30 mv','60 mv','90 mv'],loc='best')
 plt.show()
 '''
+
+low_to_high_df = tsla_df.iloc[tsla_df[
+    (tsla_df.close>tsla_df.open)&(tsla_df.key!=(tsla_df.shape[0]-1))].key.values+1]
+change_ceil_floor  = np.where(low_to_high_df['p_change']>0,
+                              np.ceil(
+                                  low_to_high_df['p_change']),
+                              np.floor(
+                                  low_to_high_df['p_change']))
+change_ceil_floor = pd.Series(change_ceil_floor)
+print('低开高收的下一个交易日所有下跌的跌幅取整和sum：'+str(
+    change_ceil_floor[change_ceil_floor<0].sum()))
+print('低开高收的下一个交易日所有上涨的跌幅取整和sum：'+str(
+    change_ceil_floor[change_ceil_floor>0].sum()))
+
+_,axs = plt.subplots(nrows=2,ncols=2,figsize=(12,10))
+change_ceil_floor.value_counts().plot(kind='bar',ax=axs[0][0])
+change_ceil_floor.value_counts().plot(kind='barh',ax=axs[0][1])
+change_ceil_floor.value_counts().plot(kind='kde',ax=axs[1][0])
+change_ceil_floor.value_counts().plot(kind='pie',ax=axs[1][1])
+plt.show()
+
