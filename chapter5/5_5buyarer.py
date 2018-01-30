66c560c4ac702c70c01ff2e22916be478695adfd
zb3215286@hotmail.com <3219286a>
2018-01-30 15:39:41 +0800

    5.5.1for buy and add label

--- chapter5/5_5buyarer.py
+++ chapter5/5_5buyarer.py
@@ -17,25 +17,45 @@ def plot_demo(axs = None,just_series = False):
     plt.ylabel('close')
     plt.title('TSLA CLOSE')
     plt.grid(True)
+    '''
+    老版本
 def plot_trade(buy_date,sell_date):
     start = tsla_df[tsla_df.index == buy_date].key.values[0]
     end = tsla_df[tsla_df.index == sell_date].key.values[0]
-
     plot_demo(just_series=True)
     plt.fill_between(tsla_df.index[start:end],0,tsla_df['close'][start:end],color='green',alpha=0.38)
     plt.ylim(np.min(tsla_df['close'])-5,
              np.max(tsla_df['close'])+5)
     plt.legend(['close'],loc='best')
-def plot_trade_with_annotate(buy_date,sell_date,anntate):
+    '''
+def plot_trade(buy_date,sell_date):
+    start = tsla_df[tsla_df.index == buy_date].key.values[0]
+    end = tsla_df[tsla_df.index == sell_date].key.values[0]
+    plot_demo(just_series=True)
+    #底色蓝色
+    plt.fill_between(tsla_df.index,0,tsla_df['close'],color='blue',alpha=0.08)
+    #如果赚钱是红色，亏钱就是绿色
+    if tsla_df['close'][end]<tsla_df['close'][start]:
+        plt.fill_between(tsla_df.index[start:end],0,tsla_df['close'][start:end],color='green',alpha=0.38)
+        is_win=False
+    else:
+        plt.fill_between(tsla_df.index[start:end], 0, tsla_df['close'][start:end], color='red', alpha=0.38)
+        is_win = True
+
+    plt.ylim(np.min(tsla_df['close'])-5,
+             np.max(tsla_df['close'])+5)
+    plt.legend(['close'],loc='best')
+    return is_win
+def plot_trade_with_annotate(buy_date,sell_date):
     '''
         :param buy_date: 交易买入日期
         :param sell_date: 交易卖出日期
         :param annotate: 卖出原因
         :return:
     '''
-    plot_trade(buy_date,sell_date)
-    plt.annotate(anntate,xy=(sell_date,tsla_df['close'].asof(sell_date)),arrowprops=dict(facecolor='yellow'),
+    is_win=plot_trade(buy_date,sell_date)
+    plt.annotate('sell for stop win'if is_win else 'sell for stop loss',xy=(sell_date,tsla_df['close'].asof(sell_date)),arrowprops=dict(facecolor='yellow'),
                  horizontalalignment='left',verticalalignment='top')
-plot_trade_with_annotate('2017-02-01','2017-05-05','sell for stop loss')
+plot_trade_with_annotate('2017-02-1','2017-03-15')
 
 plt.show()
